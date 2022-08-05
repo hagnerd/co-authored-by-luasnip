9d9a5bacb138eb0e586e9a71703d372de516e959
Matt Hagner <matthagner@gmail.com>
2022-08-05 12:16:15 -0500

    Initial commit

---
+++ lua/co-authored-by-luasnip.lua
@@ -0,0 +1,26 @@
+local luasnip = require("luasnip")
+
+local snippet = luasnip.snippet
+local format = require("luasnip.extras.fmt").fmt
+local insert_node = luasnip.insert_node
+
+local co_authored_by = format("Co-Authored-By: {}", {
+  insert_node(1, "Example <example@github.com>")
+})
+
+-- TODO: come back and make this the main export
+local function setup(opts)
+  local trigger = opts.trig or "cab"
+
+  return luasnip.add_snippets("gitcommit", {
+    snippet(
+      {
+        trig = trigger,
+        descr = {'[Co-Authored-By]', 'Support for easily inserting Co-Authored-By: <name> <handle>', 'into gitcommit message bodies'}
+      },
+      co_authored_by
+    )
+  })
+end
+
+return co_authored_by
